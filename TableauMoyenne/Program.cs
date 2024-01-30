0705cfff0a84c08608d1fced40c306df04111424
necati han <han.necati@gmail.com>
2024-01-30 16:06:18 +0100

    Handle little database for testing

---
+++ TableauMoyenne/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace TableauMoyenne
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+//#,Name,Type 1,Type 2,Total,HP,Attack,Defense,Sp. Atk,Sp. Def,Speed,Generation,Legendary
+
+            StreamReader fileReader = new StreamReader("./../../../Pokemon.csv");
+            string line;
+            int count = 0;
+            int sumAttack = 0;
+            fileReader.ReadLine();
+            while ((line = fileReader.ReadLine()) != null)
+            {
+                string[] pokemon = line.Split(',');
+                sumAttack += Convert.ToInt32(pokemon[6]);
+                count++;
+            }
+
+            float meanAttack = (float)(sumAttack * 1.0 / count);
+            Console.WriteLine($"La moyenne des attaques est de {meanAttack}");
+        }
+    }
+}
